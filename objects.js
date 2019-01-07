0c7412bb5d6985b8752ab014f3ad69308b19e89b
Brittany Malooly <bmalooly@gmail.com>
2019-01-07 03:25:39 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,10 +2,10 @@ var playlist = {
   'Ariana Grande': "thank u, next", 
   'The 1975': "Love It If We Made It", 
   'Tame Impala': "Love/Paranoia" }
-function updatePlaylist(obj, key, value) {
-  return Object.assign({}, obj, { [key]: value })
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, { [artistName]: songTitle })
 }
-function removeFromPlaylist(obj, key) {
-  delete obj[key]
-  return obj
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
   }
